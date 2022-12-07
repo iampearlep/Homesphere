4fb9e79c38392dbb0be65ff3615d725c1ade60c9
iampearlep <damilolaprincess@gmail.com>
2022-12-07 14:50:53 +0100

    update cta and footer

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
     <div className=' flex justify-center items-center container mx-auto mt-32 w-10/12'>
       <div className=' cta flex flex-col w-11/12 px-32 py-12 items-center text-center rounded-2xl border-8 border-tertiary '>
         <h2 className='text-white text-4xl font-bold mb-6'>Get started with Homesphere</h2>
-        <p className='w-1/2 text-blue-300 text-md mb-6'>Subscribe and find super attractive price quotes from us, Find your residence soon</p>
+        <p className='w-1/2 text-tertiary text-md mb-6'>Subscribe and find super attractive price quotes from us, Find your residence soon</p>
         <button className='text-md text-white font-semibold px-8 py-2 bg-tertiary mt-4 rounded-md shadow-2xl border-2 border-white'>Get Started</button>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container mx-auto mt-32 w-10/12'>
+   <div className='flex justify-between mb-16'>
+   <div className='  text-primary space-y-4'>
+        <h2 className='text-2xl font-bold'>Homesphere</h2>
+        <p className='text-sm w-7/12'>Our vision is to make all people
+          the best place to live for them
+        </p>
+      </div>
+      <div className='flex space-x-12'>
+        <div className='space-y-4 text-primary'>
+          <h3 className='text-lg font-semibold'>About</h3>
+          <p className='text-xs'>About Us</p>
+          <p className='text-xs'>Features</p>
+          <p className='text-xs'>News & Blog</p>
+        </div>
+        <div className='space-y-4 text-primary'>
+          <h3 className='text-lg font-semibold'>Company</h3>
+          <p className='text-xs'>How We Work</p>
+          <p className='text-xs'>Capital</p>
+          <p className='text-xs'>Security</p>
+        </div>
+        <div className='space-y-4 text-primary'>
+          <h3 className='text-lg font-semibold'>Support</h3>
+          <p className='text-xs'>FAQs</p>
+          <p className='text-xs'>Support Center</p>
+          <p className='text-xs'>Contact Us</p>
+        </div>
+        <div className='space-y-4 text-primary'>
+          <h3 className='text-lg font-semibold'>Movement</h3>
+          <p className='text-xs'>What Homesphere?</p>
+          <p className='text-xs'>Support Us</p>
+        </div>
+      </div>
+   </div>
+   <div className='flex justify-between mb-5 text-primary text-xs'>
+    <p>&copy; 2022 Homesphere @iampearlep. All Rights Reserved</p>
+    <p>Terms & Agreements <span>Privacy policy</span></p>
+   </div>
+    </div>
   )
 }
 
